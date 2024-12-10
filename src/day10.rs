0da48277373bae0b9d3b87fa704704ec10d80778
jussisaurio <jussi.saurio@gmail.com>
2024-12-10 16:31:29 +0200

    performance golf some more

--- src/day10.rs
+++ src/day10.rs
@@ -1,66 +1,68 @@
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 use crate::util::*;
 use std::collections::VecDeque;
 
 const CARDINAL_DIRECTIONS: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 
-fn bfs(grid: &Vec<Vec<u8>>, start: (isize, isize), is_part_2: bool) -> usize {
-    let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
+fn bfs(grid: &Vec<u8>, start: isize, width: usize, height: usize, is_part_2: bool) -> usize {
+    let mut visited = vec![false; grid.len()];
     let mut queue = VecDeque::new();
     queue.push_back((start, 0));
     let mut result = 0;
 
-    while let Some(((current_y, current_x), current_value)) = queue.pop_front() {
-        if visited[current_y as usize][current_x as usize] {
+    while let Some((current, current_value)) = queue.pop_front() {
+        if visited[current as usize] {
             continue;
         }
-        visited[current_y as usize][current_x as usize] = true;
+        visited[current as usize] = true;
         result += (current_value == 9) as usize;
+
+        let current_x = current % (width as isize);
+        let current_y = current / (width as isize);
 
         for (dx, dy) in CARDINAL_DIRECTIONS {
             let nx = current_x + dx;
             let ny = current_y + dy;
+            let n_idx = ny * (width as isize) + nx;
             let need_to_visit = ny >= 0
-                && ny < grid.len() as isize
+                && ny < height as isize
                 && nx >= 0
-                && nx < grid[0].len() as isize
-                && !visited[ny as usize][nx as usize]
-                && grid[ny as usize][nx as usize] == current_value + 1;
+                && nx < width as isize
+                && !visited[n_idx as usize]
+                && grid[n_idx as usize] == current_value + 1;
             if need_to_visit {
-                queue.push_back(((ny, nx), current_value + 1));
+                queue.push_back((n_idx, current_value + 1));
             }
         }
 
         if is_part_2 {
-            visited[current_y as usize][current_x as usize] = false;
+            visited[current as usize] = false;
         }
     }
     result
 }
 
 pub fn day10(is_part_2: bool) -> usize {
-    let input = aoc_read_day_lines(10)
+    let mut height = 1;
+    let mut width = 0;
+    let input = aoc_read_day_bytes(10)
         .iter()
-        .map(|line| {
-            line.chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
-                .collect()
-        })
-        .collect::<Vec<Vec<u8>>>();
-
-    let mut starting_points = vec![];
-    for y in 0..input.len() {
-        for x in 0..input[y].len() {
-            if input[y][x] == 0 {
-                starting_points.push((y as isize, x as isize));
+        .filter_map(|byte| {
+            width += (height == 1 && *byte != b'\n') as usize;
+            height += (*byte == b'\n') as usize;
+            if *byte == b'\n' {
+                return None;
             }
-        }
-    }
+            Some(byte - b'0')
+        })
+        .collect::<Vec<u8>>();
 
-    starting_points
+    input
         .par_iter()
-        .map(|start| bfs(&input, *start, is_part_2))
+        .enumerate()
+        .filter(|(_, &byte)| byte == 0)
+        .map(|(start, _)| bfs(&input, start as isize, width, height, is_part_2))
         .sum()
 }
 
